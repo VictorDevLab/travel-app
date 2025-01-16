a0e8df95c770c3afc89121fbd449c49e991c88ba
Victor Koome <koome.v@nathandigital.com>
2025-01-16 15:58:16 +0300

    create server

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,22 @@ const http = require('http')
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.end("<h1>Hello world!</h1>")
+   const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+
+   switch(path) {
+    case '':
+    res.writeHead(200, {"Content-Type": "text/plain"})
+    res.end("HomePage!")
+    break
+    case '/about':
+    res.writeHead(200, {"Content-Type": "text/plain"})
+    res.end("About!")
+    break
+    default:
+    res.writeHead(404, {"Content-Type": "text/plain"})
+    res.end("Not Found!")
+    break
+   }
 })
 
 server.listen(port, ()=> {
